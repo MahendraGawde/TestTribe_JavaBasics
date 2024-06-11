fcae914b97cd69fc2b94aa3ce49648d914f0e31d
Mahendra <you@example.com>
2024-06-11 18:18:31 +0530

    TT Public default protected private modifier example code commit1

--- src/main/java/PrivateModifierDemo/pkg1/Class2.java
+++ src/main/java/PrivateModifierDemo/pkg1/Class2.java
@@ -5,8 +5,8 @@ public class Class2 {
 
     //Private members from different class in same package not accessible.
     Class1 c1 = new Class1();
-    public void method2(){
+   /* public void method2(){
         System.out.println(c1.x);
         c1.method1();
-    }
+    }*/
 }
