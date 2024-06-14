4af46b6bc092d0008104f680a3c1ea1bbf3f2ba5
Mahendra <you@example.com>
2024-06-14 16:26:50 +0530

    Abstraction Demo updated

--- src/main/java/OOPsDemo/abstraction/AbstractionDemo.java
+++ src/main/java/OOPsDemo/abstraction/AbstractionDemo.java
@@ -2,6 +2,11 @@ package OOPsDemo.abstraction;
 
 public class AbstractionDemo {
     public static void main(String[] args) {
+        /* if you can check hover over methods of dog it shows package from which it inherits its properties
+        in case of creating interface reference a1 it will point to animal package while
+        for dog d1 reference method walk it, it will point to dog package since dog class have only one method
+        it will give priority to class method first then parent method.
+         */
         Animal a1 = new Dog();
         a1.eat();
         a1.walk();
