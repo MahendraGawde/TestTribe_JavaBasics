b9b89084e18a206bcfcce1a2598856925044489e
Mahendra <you@example.com>
2024-07-02 14:01:30 +0530

    SwapString commit 2

--- src/main/java/interviewPreparation/SwapStrings.java
+++ src/main/java/interviewPreparation/SwapStrings.java
@@ -12,10 +12,10 @@ public class SwapStrings {
         // Concatenate str1 and str2 and assign it to str1
         str1 = str1 + str2;
 
-        // Extract the original str2 from the concatenated string and assign it to str1
+        // Extract the original str2 from the concatenated string and assign it to str2
         str2 = str1.substring(0, str1.length() - str2.length());
 
-        // Extract the original str1 from the concatenated string and assign it to str2
+        // Extract the original str1 from the concatenated string and assign it to str1
         str1 = str1.substring(str2.length());
 
         System.out.println("After swapping:");
