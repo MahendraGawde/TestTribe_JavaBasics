90e7fbf260028a31481dd9d5ce5ebd9d927a8bba
mahendragawde <mahi.gawde011@gmail.com>
2024-08-01 22:38:27 +0530

    Array interview examples code 1 Aug 2024

---
+++ src/main/java/interviewPreparation/MissingNumberInArray.java
@@ -0,0 +1,29 @@
+package interviewPreparation;
+
+public class MissingNumberInArray {
+    public static void main(String[] args) {
+        int[] arr = {1,4,9,25};
+        int missingNumber = findMissingNumber(arr);
+        System.out.println("Missing number is: " +missingNumber);
+
+    }
+
+    public static int findMissingNumber(int[] arr){
+        int expectedNo = 1;
+        int index = 0;
+
+        while(index < arr.length){
+            int square = expectedNo * expectedNo;
+
+            if(arr[index] != square){
+                // If the current array value doesn't match the expected square, return the square
+                return square;
+            }
+            // Move to the next number and array index
+            expectedNo++;
+            index++;
+        }
+        // If no missing number was found in the loop, calculate the next expected square
+        return expectedNo * expectedNo;
+    }
+}
---
+++ src/main/java/interviewPreparation/SecondHighestInArray.java
@@ -0,0 +1,35 @@
+package interviewPreparation;
+
+public class SecondHighestInArray {
+    public static void main(String[] args) {
+        int[] arr = {5,5,5};
+        try {
+            int secondHighest = findSecondHighest(arr);
+            System.out.println("Second Highest number in array is: " +secondHighest);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static int findSecondHighest(int[] arr){
+        if(arr.length < 2){
+            throw new IllegalArgumentException("Array must contain at least 2 elements");
+        }
+        int highest = Integer.MIN_VALUE;
+        int secondHighest =Integer.MIN_VALUE;
+
+        for (int num : arr) {
+            if (num > highest) {
+                secondHighest = highest;
+                highest = num;
+            } else if (num > secondHighest && num != highest) {
+                secondHighest = num;
+            }
+        }
+            if(secondHighest == Integer.MIN_VALUE){
+                throw new IllegalArgumentException("There is no second highest element in array.");
+            }
+            return secondHighest;
+        }
+
+}
