6e3b31823c79012a54869bc4838133c527e931be
mahendragawde <mahi.gawde011@gmail.com>
2024-08-21 20:15:07 +0530

    interview question repeated char

--- src/main/java/interviewPreparation/Practice.java
+++ src/main/java/interviewPreparation/Practice.java
@@ -1,12 +1,25 @@
 package interviewPreparation;// Online Java Compiler
 // Use this editor to write, compile and run your Java code online
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 class Practice {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("Enter number to reverse: ");
-        int num = input.nextInt();
+
+        int num = 0;
+        boolean validInput = false;
+
+        while(!validInput){
+            try{
+                System.out.print("Enter number to reverse: ");
+                num = input.nextInt();
+                validInput = true; //if input is valid then exit loop
+            }catch(InputMismatchException e){
+                System.out.println("Invalid input! Please enter a valid integer.");
+                input.next(); //clear the invalid input.
+            }
+        }
         System.out.println("Reverse of " +num+ " is " +reverse(num));
 
     }
---
+++ src/main/java/interviewPreparation/RepeatedCharacters.java
@@ -0,0 +1,34 @@
+package interviewPreparation;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RepeatedCharacters {
+    public static void main(String[] args) {
+        String input = "Programming in java";
+        countRepeatedChars(input);
+    }
+
+    public static void countRepeatedChars(String str){
+        // Create hashmap to store character counts
+        Map<Character, Integer> charCountMap = new HashMap<>();
+
+        //Convert string to character array
+        char[] charArray = str.toCharArray();
+
+        //Iterate over character array
+        for(char c : charArray){
+            if(c == ' '){
+                continue;
+            }
+            charCountMap.put(c, charCountMap.getOrDefault(c,0) +1);
+        }
+        System.out.println("Repeated characters in string: ");
+
+        for(Map.Entry<Character,Integer> entry : charCountMap.entrySet()){
+            if(entry.getValue() > 1){
+                System.out.println(entry.getKey() + " : " + entry.getValue() +" times");
+            }
+        }
+    }
+}
