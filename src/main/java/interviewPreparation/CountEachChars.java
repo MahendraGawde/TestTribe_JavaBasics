921cfe0fad74a8f3ccf25f7fe96246ebf509f8e8
mahendragawde <mahi.gawde011@gmail.com>
2024-11-07 16:12:26 +0530

    Count charc update

--- src/main/java/interviewPreparation/CountEachChars.java
+++ src/main/java/interviewPreparation/CountEachChars.java
@@ -6,30 +6,25 @@ import java.util.Map;
 public class CountEachChars {
 
     public static void main(String args[]) {
-        String input = "rishi";
+        String input = "rishi raj";
         Map<Character, Integer> charCountMap = countChars(input);
 
         // Print the character counts
-        for (Map.Entry<Character, Integer> entry : charCountMap.entrySet()) {
-            System.out.println("Character '" + entry.getKey() + "' occurs " + entry.getValue() + " times.");
-        }
+        charCountMap.forEach((key, value) ->
+                System.out.println("Character '" + key + "' occurs " + value + " times.")
+        );
     }
 
-    public static Map<Character,Integer> countChars(String str){
+    public static Map<Character, Integer> countChars(String str) {
         Map<Character, Integer> countChars = new HashMap<>();
-
         str = str.toLowerCase();
-        char[] charArray = str.toCharArray();
 
-        for(char c : charArray){
-            if (countChars.containsKey(c)){
-                countChars.put(c, countChars.get(c) +1);
-
-            }else{
-                countChars.put(c,1);
+        for (char c : str.toCharArray()) {
+            if (c != ' ') {  // Skip spaces
+                countChars.put(c, countChars.getOrDefault(c, 0) + 1);
             }
         }
-        return countChars;
 
+        return countChars;
     }
 }
