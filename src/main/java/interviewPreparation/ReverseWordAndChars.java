28d4f1c0bc04631ae6548422d48bddab7d9f102e
mahendragawde <mahi.gawde011@gmail.com>
2024-11-10 02:38:00 +0530

    10 Nov code

---
+++ src/main/java/interviewPreparation/ReverseWordAndChars.java
@@ -0,0 +1,22 @@
+package interviewPreparation;
+
+public class ReverseWordAndChars {
+    public static void main(String[] args) {
+        String input = "Hello World";
+        String result = reverseWordAndChars(input);
+        System.out.println("Original String: " +input);
+        System.out.println("Reversed word and characters: " +result);
+    }
+
+    private static String reverseWordAndChars(String input) {
+        String[] words = input.split(" ");
+        StringBuilder reversedString = new StringBuilder();
+
+        for (int i = words.length-1; i >=0 ; i--) {
+            StringBuilder reversedWords = new StringBuilder(words[i]).reverse();
+            reversedString.append(reversedWords).append(" ");
+        }
+        return reversedString.toString().trim();
+
+    }
+}
