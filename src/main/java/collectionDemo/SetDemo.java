e80c371541ba503a03177d86b93716f8200d8f15
mahendragawde <mahi.gawde011@gmail.com>
2024-11-27 21:35:20 +0530

    Set update

--- src/main/java/collectionDemo/SetDemo.java
+++ src/main/java/collectionDemo/SetDemo.java
@@ -7,6 +7,7 @@ import java.util.Set;
 public class SetDemo {
     public static void main(String[] args) {
         Set<String> s1 = new HashSet<String>();
+        Set<String> s2 = new HashSet<String>();
 
         s1.add("flour");
         s1.add("salt");
@@ -14,9 +15,18 @@ public class SetDemo {
         s1.add("sugar");
         s1.add("flour");
         s1.add("maggi");
+        s2.addAll(s1);
+        s2.add("test");
+
+        System.out.println(s2.size());
+        System.out.println(s1);
+        System.out.println("---------->");
+        System.out.println(s2);
 
         System.out.println(s1.size());
-        s1.remove("sugar");
+//        s1.remove("sugar");
+        s2.removeAll(s1);
+        System.out.println(s2);
         System.out.println(s1.size());
         System.out.println(s1);
 
