1e03e513456b5f9323092401e4bdbfcde989693f
mahendragawde <mahi.gawde011@gmail.com>
2024-12-03 17:23:31 +0530

    Array Programs added 3 Dec

--- src/main/java/interviewOnArrays/MissingNumberInArray.java
+++ src/main/java/interviewOnArrays/MissingNumberInArray.java
@@ -3,8 +3,8 @@ package interviewOnArrays;
 public class MissingNumberInArray {
     public static void main(String[] args) {
         int[] numbers = {1,2,4,5,6};
-        int totalArraySize = numbers.length +1;
-        int expectedSum = (totalArraySize *(totalArraySize+1))/2;
+        int n = numbers.length +1;
+        int expectedSum = n *(n+1)/2;
 
         System.out.println(expectedSum);
 
